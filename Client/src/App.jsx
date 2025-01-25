a070582f805497a70836e1396e30ed08c43f9d2d
Dipanshu Zalke <dipanshuzalke@gmail.com>
2025-01-25 18:27:59 +0530

    Changes in login and profile button

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,31 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import StudyRoom from './pages//studyRoom/StudyRoom.jsx';
-import Dashboard from './pages/Dashboard';
-import GameRoom from './pages/GameRoom';
-import Signout from './Auth/Signout';
-import SignUp from './Auth/Authenticate.jsx';
-import ProfilePage from './pages/ProfilePage.jsx';
-import PageNotFound from './pages/PageNotFound.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import StudyRoom from './pages//studyRoom/StudyRoom.jsx'
+import Dashboard from './pages/Dashboard'
+import GameRoom from './pages/GameRoom'
+import Signout from './Auth/Signout'
+import SignUp from './Auth/Authenticate.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
+import PrivateRoute from '../src/Auth/PrivateRoute.jsx'
 
+function App () {
+  const token = localStorage.getItem('token')
 
-function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<StudyRoom />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="games" element={<GameRoom />} />
-          <Route path="authenticate" element={<SignUp />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='games' element={<GameRoom />} />
+          <Route path='authenticate' element={<SignUp />} />
           {/* Unnecessary / removed. http://localhost:5173/signout loads after this, button is inactive right now */}
-          <Route path="signout" element={<Signout />} /> 
-          <Route path="profile" element={<ProfilePage />} />
-          <Route path="*" element={<PageNotFound />} />
+          <Route path='signout' element={<Signout />} />
+          {token && <Route path='profile' element={<ProfilePage />} />}
+          <Route path='*' element={<PageNotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
