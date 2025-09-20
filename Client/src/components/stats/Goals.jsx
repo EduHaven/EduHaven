091bfd517ec066e2daae9ff79437452a315b7728
itsakanksharawat <akankshaar3256@gmail.com>
2025-09-20 22:42:05 +0530

    fix

--- Client/src/components/stats/Goals.jsx
+++ Client/src/components/stats/Goals.jsx
@@ -14,7 +14,7 @@ import {
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
-} from "@/components/ui/dropdownmenu";
+} from "@/components/ui/dropdown-menu";
 import axiosInstance from "@/utils/axios";
 import { useParams } from "react-router-dom";
 
