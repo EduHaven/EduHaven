01d5d073a0d5eaf3159c62ef52c0fba194fb5576
rajmanna <dev.rajm8923@gmail.com>
2025-10-19 19:41:01 +0530

    display visibility on note

--- Client/src/components/notes/NoteFooter.jsx
+++ Client/src/components/notes/NoteFooter.jsx
@@ -1,11 +1,18 @@
 const NoteFooter = ({ note }) => {
+  // visibility map
+  const visibilityMap = {
+    public: "Public",
+    private: note.collaborators?.length ? "Shared" : "Private",
+  };
   return (
     <div
       className="text-xs mt-2 flex justify-between items-center"
       style={{ color: "var(--txt-disabled)" }}
     >
       {new Date(note?.createdAt).toLocaleDateString()}
-      <div className="rounded-md border px-3 py-1">Public</div>
+      <div className="border px-3 py-1 rounded-md">
+        {visibilityMap[note.visibility]}
+      </div>
     </div>
   );
 };
