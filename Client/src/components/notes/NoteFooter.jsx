85c19087ceee34dc00faa3612f562989f225ea88
rajmanna <dev.rajm8923@gmail.com>
2025-10-20 19:58:36 +0530

    added note owner tiny profile picture

--- Client/src/components/notes/NoteFooter.jsx
+++ Client/src/components/notes/NoteFooter.jsx
@@ -1,17 +1,38 @@
+import { fetchUserDetails } from "@/api/userApi";
+import { useEffect, useState } from "react";
+
 const NoteFooter = ({ note }) => {
-  // visibility map
-  const visibilityMap = {
-    public: "Public",
-    private: note.collaborators?.length ? "Shared" : "Private",
-  };
+  const [owner, setOwner] = useState(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const data = await fetchUserDetails(note.owner);
+      setOwner(data);
+    };
+    getUser();
+  }, [note.owner]);
+
+  // visibility
+  let visibility = "Private";
+  if (note.visibility === "public") visibility = "Public";
+  else if (note.visibility === "private" && note.collaborators?.length)
+    visibility = "Shared";
+
   return (
     <div
       className="text-xs mt-2 flex justify-between items-center"
       style={{ color: "var(--txt-disabled)" }}
     >
       {new Date(note?.createdAt).toLocaleDateString()}
-      <div className="border px-3 py-1 rounded-md">
-        {visibilityMap[note.visibility]}
+      <div className="border px-2 h-7 rounded-full flex items-center space-x-2">
+        <span>{visibility}</span>
+        {visibility === "Shared" && (
+          <img
+            className="w-5 h-5 rounded-full"
+            src={owner?.ProfilePicture}
+            alt={owner?.FirstName}
+          />
+        )}
       </div>
     </div>
   );
